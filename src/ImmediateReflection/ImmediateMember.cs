7649b5bae3ae0490ae1d152e3fba348688219c47
Alexandre Rabérin <kernelith@live.fr>
2019-05-18 19:01:01 +0200

    Expose attributes on ImmediateMember (cached).

--- src/ImmediateReflection/ImmediateMember.cs
+++ src/ImmediateReflection/ImmediateMember.cs
@@ -1,4 +1,10 @@
 using System;
+using System.Collections.Generic;
+#if SUPPORTS_LINQ
+using System.Linq;
+#else
+using static ImmediateReflection.Utils.EnumerableUtils;
+#endif
 using System.Reflection;
 using JetBrains.Annotations;
 
@@ -15,6 +21,12 @@ namespace ImmediateReflection
         [NotNull]
         public string Name { get; }
 
+        [NotNull]
+        private readonly Dictionary<Type, List<Attribute>> _attributesWithInherited;
+
+        [NotNull]
+        private readonly Dictionary<Type, List<Attribute>> _attributesWithoutInherited;
+
         /// <summary>
         /// Constructor.
         /// </summary>
@@ -26,6 +38,160 @@ namespace ImmediateReflection
                 throw new ArgumentNullException(nameof(member));
 
             Name = member.Name;
+
+            // Attributes
+#if SUPPORTS_LINQ
+            Attribute[] attributesNotInherited = member.GetCustomAttributes(false).OfType<Attribute>().ToArray();
+            Attribute[] attributesInherited = member.GetCustomAttributes(true).OfType<Attribute>().ToArray();
+#else
+            Attribute[] attributesNotInherited = ToArray(OfType<Attribute>(member.GetCustomAttributes(false)));
+            Attribute[] attributesInherited = ToArray(OfType<Attribute>(member.GetCustomAttributes(true)));
+#endif
+
+            _attributesWithoutInherited = new Dictionary<Type, List<Attribute>>(attributesNotInherited.Length);
+            FillAttributesDictionary(_attributesWithoutInherited, attributesNotInherited);
+
+            _attributesWithInherited = new Dictionary<Type, List<Attribute>>(attributesInherited.Length);
+            FillAttributesDictionary(_attributesWithInherited, attributesInherited);
+
+            #region Local function
+
+            void FillAttributesDictionary(Dictionary<Type, List<Attribute>> attributesDictionary, Attribute[] attributes)
+            {
+                foreach (Attribute attribute in attributes)
+                {
+                    Type attributeType = attribute.GetType();
+                    if (attributesDictionary.TryGetValue(attributeType, out List<Attribute> currentAttributes))
+                        currentAttributes.Add(attribute);
+                    else
+                        attributesDictionary[attributeType] = new List<Attribute> { attribute };
+                }
+            }
+
+            #endregion
+        }
+
+        /// <summary>
+        /// Retrieves a custom attribute of type <typeparamref name="TAttribute"/> that is applied to this member.
+        /// </summary>
+        /// <typeparam name="TAttribute">Attribute type.</typeparam>
+        /// <param name="inherit">Indicates if inherited attributes should be taken into account.</param>
+        /// <returns>The first attribute matching requested type, otherwise null.</returns>
+        [Pure]
+        [CanBeNull]
+        public TAttribute GetAttribute<TAttribute>(bool inherit = false)
+            where TAttribute : Attribute
+        {
+            if (inherit)
+                return FindAttribute(_attributesWithInherited);
+            return FindAttribute(_attributesWithoutInherited);
+
+            #region Local function
+
+            TAttribute FindAttribute(Dictionary<Type, List<Attribute>> attributesDictionary)
+            {
+                if (attributesDictionary.TryGetValue(typeof(TAttribute), out List<Attribute> attributes))
+                    return (TAttribute)attributes[0];
+                return null;
+            }
+
+            #endregion
+        }
+
+        /// <summary>
+        /// Retrieves a custom attribute of type <paramref name="attributeType"/> that is applied to this member.
+        /// </summary>
+        /// <param name="attributeType">Type of the attribute to search.</param>
+        /// <param name="inherit">Indicates if inherited attributes should be taken into account.</param>
+        /// <returns>The first attribute matching requested type, otherwise null.</returns>
+        /// <exception cref="ArgumentNullException">If the given <paramref name="attributeType"/> is null.</exception>
+        /// <exception cref="ArgumentException">If the given <paramref name="attributeType"/> is not an <see cref="Attribute"/> type.</exception>
+        [Pure]
+        [CanBeNull]
+        public Attribute GetAttribute([NotNull] Type attributeType, bool inherit = false)
+        {
+            if (attributeType is null)
+                throw new ArgumentNullException(nameof(attributeType));
+            if (!typeof(Attribute).IsAssignableFrom(attributeType))
+                throw new ArgumentException($"{nameof(attributeType)} must be an {nameof(Attribute)} type.");
+
+            if (inherit)
+                return FindAttribute(_attributesWithInherited);
+            return FindAttribute(_attributesWithoutInherited);
+
+            #region Local function
+
+            Attribute FindAttribute(Dictionary<Type, List<Attribute>> attributesDictionary)
+            {
+                if (attributesDictionary.TryGetValue(attributeType, out List<Attribute> attributes))
+                    return attributes[0];
+                return null;
+            }
+
+            #endregion
+        }
+
+        /// <summary>
+        /// Retrieves custom attributes of type <typeparamref name="TAttribute"/> that are applied to this member.
+        /// </summary>
+        /// <typeparam name="TAttribute">Attribute type.</typeparam>
+        /// <param name="inherit">Indicates if inherited attributes should be taken into account.</param>
+        /// <returns>Attributes matching requested type.</returns>
+        [Pure]
+        [CanBeNull]
+        public IEnumerable<TAttribute> GetAttributes<TAttribute>(bool inherit = false)
+            where TAttribute : Attribute
+        {
+            if (inherit)
+                return FindAttributes(_attributesWithInherited);
+            return FindAttributes(_attributesWithoutInherited);
+
+            #region Local function
+
+            IEnumerable<TAttribute> FindAttributes(Dictionary<Type, List<Attribute>> attributesDictionary)
+            {
+                if (attributesDictionary.TryGetValue(typeof(TAttribute), out List<Attribute> attributes))
+#if SUPPORTS_LINQ
+                    return attributes.OfType<TAttribute>();
+                return Enumerable.Empty<TAttribute>();
+#else
+                    return OfType<TAttribute>(attributes);
+                return Empty<TAttribute>();
+#endif
+            }
+
+            #endregion
+        }
+
+        /// <summary>
+        /// Retrieves custom attributes of type <paramref name="attributeType"/> that are applied to this member.
+        /// </summary>
+        /// <param name="attributeType">Type of the attribute to search.</param>
+        /// <param name="inherit">Indicates if inherited attributes should be taken into account.</param>
+        /// <returns>Attributes matching requested type.</returns>
+        [Pure]
+        [CanBeNull]
+        public IEnumerable<Attribute> GetAttributes([NotNull] Type attributeType, bool inherit = false)
+        {
+            if (inherit)
+                return FindAttributes(_attributesWithInherited);
+            return FindAttributes(_attributesWithoutInherited);
+
+            #region Local function
+
+            IEnumerable<Attribute> FindAttributes(Dictionary<Type, List<Attribute>> attributesDictionary)
+            {
+                if (attributesDictionary.TryGetValue(attributeType, out List<Attribute> attributes))
+#if SUPPORTS_LINQ
+                    return attributes.AsEnumerable();
+                return Enumerable.Empty<Attribute>();
+#else
+                    return AsEnumerable(attributes);
+                return Empty<Attribute>();
+#endif
+            }
+
+            #endregion
         }
     }
 }
