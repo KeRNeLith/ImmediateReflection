d2d297377791c2e39188bd6652459abe1dd3c1de
Alexandre Rabérin <kernelith@live.fr>
2025-10-08 23:31:11 +0200

    Add some polyfills implementations for old targets support of modern C# attributes/features.

---
+++ src/ImmediateReflection.CommonInternals/Polyfills/Polyfills.cs
@@ -0,0 +1,26 @@
+﻿#if NETFRAMEWORK || NETSTANDARD2_0
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Parameter)]
+    internal sealed class NotNullWhenAttribute : Attribute
+    {
+        public NotNullWhenAttribute(bool returnValue)
+        {
+            ReturnValue = returnValue;
+        }
+
+        public bool ReturnValue { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue)]
+    internal sealed class NotNullIfNotNullAttribute : Attribute
+    {
+        public NotNullIfNotNullAttribute(string parameterName)
+        {
+            ParameterName = parameterName;
+        }
+
+        public string ParameterName { get; }
+    }
+}
+#endif
